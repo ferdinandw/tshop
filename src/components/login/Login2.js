2d5b40e618e6c2b2b100bfa0add1135b60645eaa
ferdinandw <fwng123@gmail.com>
2020-05-23 03:03:57 +0700

    register

--- src/components/login/Login2.js
+++ src/components/login/Login2.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { withRouter} from "react-router-dom";
+import { withRouter , Link} from "react-router-dom";
 
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
@@ -101,6 +101,7 @@ class Login extends Component {
                                                 invalid: errors.password || errors.passwordincorrect
                                             })}/>
                                         </div>
+                                        <h6>Don't have an Account ? <Link to="/register">Sign Up Here</Link></h6>
                                         <div style={{margin:20}} className="text-center pt-4">
                                             <button type="submit" className="btn btn-outline-dark">Login</button>
                                         </div>
